2e5108f47f77b044f196cee47e359a0a5cc9acdc
Faisal <faizizz0912@gmail.com>
2025-07-03 15:51:41 +0700

    Landing Page

---
+++ views/home/index.php
@@ -0,0 +1,31 @@
+<div class="container-fluid" style="background-color: #C0C9EE;">
+    <div class="container pt-5 pb-2">
+        <div class="row">
+            <div class="col">
+                <h1>SMK PANCASILA 1 KUTOARJO</h1>
+                <p>Adalah Sekolah Teknik yang mencetak penerus bangsa yang berkompetensi</p>
+                <button type="button" class="btn btn-primary"> Daftar Sekarang </button>
+                <button type="button" class="btn btn-primary"> Video </button>
+                <p style="font-size: 13px; position: absolute; top: 320px; right: 1045px;">We are in socials media</p>
+
+
+            </div>
+            <div class="col">
+                <img src="https://media.istockphoto.com/id/185273956/id/foto/koridor-sekolah-menengah-modern.jpg?s=612x612&w=0&k=20&c=01W2c6ILL-bMS07fhX5ksnCY9fzKueXheFQdGpyj7-o=" class="rounded" width="350" />
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+</div>
+br
+
+
+
+
+
+
+
+
+
+
